26b4a08ee51394d7db18f6a927500f271c19c712
Alejandro Eyquem <aeyquem@live.cl>
2020-06-03 23:06:36 +1000

    changed folder structure and added menu-item and directory-menu components

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homePage.styles.scss';
+import DirectoryMenu from '../../components/directory-menu/DirectoryMenu'
+
+const HomePage = () => {
+
+    return (
+        <div className="homepage">
+            <DirectoryMenu />
+        </div>
+    );
+
+
+}
+
+export default HomePage;
